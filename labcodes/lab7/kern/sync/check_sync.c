caa450d5d7406520d42ab3093233a0e6127141af
LuChengTHU <c-lu15@mails.tsinghua.edu.cn>
2018-03-25 22:12:28 +0800

    Finish Lab7

--- labcodes/lab7/kern/sync/check_sync.c
+++ labcodes/lab7/kern/sync/check_sync.c
@@ -179,30 +179,39 @@ void phi_test_condvar (i) {
 
 
 void phi_take_forks_condvar(int i) {
-     down(&(mtp->mutex));
+    down(&(mtp->mutex));
 //--------into routine in monitor--------------
-     // LAB7 EXERCISE1: YOUR CODE
-     // I am hungry
-     // try to get fork
+    // LAB7 EXERCISE1: YOUR CODE
+    // I am hungry
+    state_condvar[i]=HUNGRY; 
+    // try to get fork
+    phi_test_condvar(i); 
+    if (state_condvar[i] != EATING) {
+        cprintf("phi_take_forks_condvar: %d didn't get fork and will wait\n",i);
+        cond_wait(&mtp->cv[i]);
+    }
 //--------leave routine in monitor--------------
-      if(mtp->next_count>0)
-         up(&(mtp->next));
-      else
-         up(&(mtp->mutex));
+    if(mtp->next_count>0)
+        up(&(mtp->next));
+    else
+        up(&(mtp->mutex));
 }
 
 void phi_put_forks_condvar(int i) {
-     down(&(mtp->mutex));
+    down(&(mtp->mutex));
 
 //--------into routine in monitor--------------
-     // LAB7 EXERCISE1: YOUR CODE
-     // I ate over
-     // test left and right neighbors
+    // LAB7 EXERCISE1: YOUR CODE
+    // I ate over 
+    state_condvar[i]=THINKING;
+    // test left and right neighbors
+    phi_test_condvar(LEFT);
+    phi_test_condvar(RIGHT);
 //--------leave routine in monitor--------------
-     if(mtp->next_count>0)
-        up(&(mtp->next));
-     else
-        up(&(mtp->mutex));
+    if(mtp->next_count>0)
+    up(&(mtp->next));
+    else
+    up(&(mtp->mutex));
 }
 
 //---------- philosophers using monitor (condition variable) ----------------------
